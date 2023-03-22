859c2ad6da5b86d7d21f96567ad6dd14ef291678
eudemon000 <eudemon000@gmail.com>
2023-03-22 19:28:58 +0800

    aaa

--- gitbat.bat
+++ gitbat.bat
@@ -1,12 +1,4 @@
-:: input commit message
-set /p commit_msg=Please input commit message:
-:: show git status
-git status
-:: add all changing
-git add -A
-:: local commit
-git commit -m "%commit_msg%"
-:: push to remote repository
-git push origin master
-:: make a pause 
-pause
+@echo
+git commit -a
+git push -u gitee_origin master
+git push -u github_origin master
